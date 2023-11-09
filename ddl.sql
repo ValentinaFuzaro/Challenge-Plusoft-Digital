3fb90248a06e85a722616943390705563fcabfe8
ValentinaFuzaro <101301335+ValentinaFuzaro@users.noreply.github.com>
2023-11-09 13:22:36 -0300

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,1 +1,16 @@
+CREATE TABLE feedback (
+    feedback_id BIGINT PRIMARY KEY,
+    description_feedback VARCHAR(255),
+    date_feedback DATE
+);
+
+CREATE TABLE usuario (
+    id_usuario BIGINT PRIMARY KEY,
+    name_user VARCHAR(255),
+    email VARCHAR(255),
+    senha VARCHAR(255),
+    cellphone_user VARCHAR(20),
+    company_user VARCHAR(255),
+    date_user DATE
+);
 
